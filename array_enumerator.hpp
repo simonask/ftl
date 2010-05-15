cfb829de9e26a06ddec3b8f76ff90682739f5b3f
Simon Ask Ulsnes <simon@ulsnes.dk>
2010-05-15 18:03:29 +0200

    fixed ArrayEnumerator

--- array_enumerator.hpp
+++ array_enumerator.hpp
@@ -7,13 +7,14 @@ namespace ftl {
 	class ArrayEnumerator {
 	public:
 		typedef T ValueType;
-		LinearEnumerator(T* begin, T* end) : _end(end), _current(begin-1) {}
-		LinearEnumerator(const LinearEnumerator& other) : _end(other._end), _current(other._current) {}
+		ArrayEnumerator() : _end(NULL), _current(NULL) {}
+		ArrayEnumerator(T* begin, T* end) : _end(end), _current(begin-1) {}
+		ArrayEnumerator(const ArrayEnumerator& other) : _end(other._end), _current(other._current) {}
 		bool move_next() { ++_current; return !at_end(); }
 		T current() { return *_current; }
 		bool at_end() const { return _current >= _end; }
 		const T& current() const { return *_current; }
-		const LinearEnumerator<T>& get_enumerator() const { return *this; }
+		const ArrayEnumerator<T>& get_enumerator() const { return *this; }
 		size_t size() const { return _end - _current; }
 	private:
 		T* _current;

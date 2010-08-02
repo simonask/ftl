c7f2480f6ff6c2a69f35d293f9918eef1d6e35b3
Simon Ask Ulsnes <simon@ulsnes.dk>
2010-08-02 23:22:36 +0200

    crude array test test

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,20 @@
+#include "test.hpp"
 #include "print.hpp"
+#include "array.hpp"
 
 using namespace ftl;
 
 int main (int argc, char const *argv[])
 {
+	TEST_SUITE("Array", {
+		TEST_CASE("push", {
+			Array<int> a;
+			a.push(123);
+			
+			TEST(a.size() == 1);
+			TEST(a[0] == 123);
+		});
+	});
 	auto str = format("A number: {0}, the same number: {0}, something else: {1}", 123, "hejsa");
 	print("{0}\n", str);
 	return 0;
